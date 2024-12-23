a6684f63a2f1c4df9be116855cc0cd0fb3767c77
student3B032081 <3b032081@gm.student.ncut.edu.tw>
2024-12-23 22:09:53 +0800

    練習5

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,12 @@ Route::get('hello/{name}', function ($name='Everybody') {
     return 'Hello, ' .$name;
 })->name('hello.index');
 
+Route::get('dashboard', function() {
+    return 'dashboard';
+});
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('dashboard', function () {
+        return 'admin dashboard';
+    });
+});
