dc7cabf370fe2f36af2e7c37ce44f17b045cd058
student3B032081 <3b032081@gm.student.ncut.edu.tw>
2024-12-23 21:53:01 +0800

    練習2

--- routes/web.php
+++ routes/web.php
@@ -22,4 +22,10 @@ Route::get('r2', function () {
 Route::get('r1', function () {
     return redirect('r2');
 });
+Route::get('hello/{name}', function ($name) {
+    return 'Hello, ' .$name;
+});
+Route::get('hello/{name}', function ($name='Everybody') {
+    return 'Hello, ' .$name;
+});
 
