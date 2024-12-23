3bc3d83086fe1040ef64e24571f70a55eaff1621
student3B032081 <3b032081@gm.student.ncut.edu.tw>
2024-12-23 21:41:00 +0800

    設定Route跳轉路由

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return 'welcome';
 });
+Route::get('r2', function () {
+    return view('welcome');
+});
+Route::get('r1', function () {
+    return redirect('r2');
+});
+
